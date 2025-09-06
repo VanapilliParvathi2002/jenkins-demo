92ed56ebbfe0c53c45aec4526f4d28c997e6448a
Vanapilli Parvathi <vanapilliparvathi@gmail.com>
2025-09-07 01:18:59 +0530

    Added Java HelloWorld project

---
+++ sample_java_project/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void shouldCreateApp() {
+        App app = new App();
+        assertNotNull(app);
+    }
+}
